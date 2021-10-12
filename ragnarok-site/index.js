4989d73c86e45048382e45710a9a307cd28a2e2c
ErickMacena <erickmacena2@gmail.com>
2021-10-12 20:54:33 -0300

    Base Style Added

--- ragnarok-site/index.js
+++ ragnarok-site/index.js
@@ -1,1 +1,18 @@
 console.log(data)
+
+function loadItemsOnScreen(data=[]) {
+    data.map(item => {
+        return `<li class="flex-row p-1">
+        <div>
+            <img src="${item.image}"
+                alt="" width="120px">
+        </div>
+
+        <div class="p-1">
+            <h4 class="fs-2">${item.name}</h4>
+            <p class="fs-2">${item.description}</p>
+            <h4 class="color-black-2 fs-3">R$ ${item.value}</h4>
+        </div>
+    </li>`
+    })
+}
