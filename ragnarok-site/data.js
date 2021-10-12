4c8e1960f41899504db81f5a3d228d326ccb2528
ErickMacena <erickmacena2@gmail.com>
2021-10-12 19:11:38 -0300

    few configurations on css

---
+++ ragnarok-site/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    "id": "f1cb6682-25ab-4d53-8014-3f4b322a73c3",
+    "name": "Chocolate",
+    "description": "O melhor chocolate",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zh7DvSItDsIB3bprK9pakQHaFS%26pid%3DApi&f=1",
+    "value": "75"
+  },
+  {
+    "id": "923b40b2-5ba0-4cd3-9051-8b8cd3188c8a",
+    "name": "Arroz",
+    "description": "Arroz de boa qualidade",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zh7DvSItDsIB3bprK9pakQHaFS%26pid%3DApi&f=1",
+    "value": "9.9"
+  },
+  {
+    "id": "2dc7ebe1-8437-404d-9edb-d57e58a13e53",
+    "name": "Pipoca",
+    "description": "Você gosta de pipoca?",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zh7DvSItDsIB3bprK9pakQHaFS%26pid%3DApi&f=1",
+    "value": "15"
+  },
+  {
+    "id": "cb9c9676-a20f-437e-9b80-7654657e783e",
+    "name": "Queijo",
+    "description": "Trem bão!",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6oxkoVvKKPTPvazXxD-juAHaHa%26pid%3DApi&f=1",
+    "value": "30"
+  },
+  {
+    "id": "6d8e6254-bf46-4f8c-8641-89077edbff30",
+    "name": "teste",
+    "description": "Você gosta de pipoca?",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zh7DvSItDsIB3bprK9pakQHaFS%26pid%3DApi&f=1",
+    "value": "15"
+  }
+]
