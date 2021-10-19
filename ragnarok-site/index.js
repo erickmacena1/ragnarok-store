9dcaefb3116836a585e20d8cd0786aa87bcc9705
ErickMacena <erickmacena2@gmail.com>
2021-10-19 17:34:23 -0300

    Data now comes from API

--- ragnarok-site/index.js
+++ ragnarok-site/index.js
@@ -1,26 +1,47 @@
 const $productList = document.querySelector("#productList")
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadItemsOnScreen(data)
-    Array.from(document.querySelectorAll(".buy-btn"))
-        .forEach(btn => btn.onclick = handleOrder)
+document.addEventListener('DOMContentLoaded', async () => {
+    getAllProducts().then(productList => {
+        console.log(productList)
+        loadItemsOnScreen(productList)
+
+        Array.from(document.querySelectorAll(".buy-btn"))
+            .forEach(btn => btn.onclick = () => handleOrder(btn.parentNode));
+    })
+
+    Array.from(document.querySelectorAll(".modal-switch"))
+        .forEach(modalBtn => {
+            modalBtn.onclick = () => switchModal(modalBtn)
+        })
 })
-console.log("Dados:")
-console.log(data)
+
+async function getAllProducts() {
+    let products
+
+    try {
+        products = await fetch('https://ragnarok-app.herokuapp.com/api/v1')
+
+        return await products.json()
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 function loadItemsOnScreen(data = []) {
     const productList = data.map(item => {
         return `<li class="flex-row align-center p-1">
         <div>
-            <img src="${item.image}"
+            <img src="${item.image.url}"
                 alt="" width="120px">
         </div>
 
         <div id=${item.id} class="p-1">
-            <h4 class="fs-2">${item.name}</h4>
+            <h4 class="fs-2 color-black-2">${item.name}</h4>
             <p class="fs-2">${item.description}</p>
-            <h4 class="color-black-2 fs-3">R$ ${item.value}</h4>
-            <button class="fs-2 buy-btn m-05">Comprar</button>
+            <h4 class="color-black-1 fs-3">R$ ${item.value}</h4>
+            <button class="fs-2 btn buy-btn m-05 color-black-2">Comprar</button>
         </div>
 
     </li>`
@@ -29,22 +50,20 @@ function loadItemsOnScreen(data = []) {
     productList.forEach(li => $productList.innerHTML += li)
 }
 
-function handleOrder(e) {
+async function handleOrder(productElem) {
 
-    console.log("BUY ORDER")
-    console.log(e.target.parentNode.id)
+    const productId = productElem.id
+    console.log(productId)
 
-    // var stripe = Stripe('pk_test_51JV0dAHIXjsNwm1Ce0ztCpoqq6wltPkOV2TeamoJNVFoQqXI4CIFYtnGymz8UfnYlQQfL2vxVoKJxzPKD53ZbRBK00Gqz50bUf')
+    fetch(`https://ragnarok-app.herokuapp.com/api/v1/checkout/${productId}`, {
+        method: "GET",
+        mode: 'cors',
+    })
+        .then(res => res.json())
+        .then(data => window.location.href = data.checkoutUrl)
 
-    // var orderBtn = document.getElementById('order-btn')
+}
 
-    // orderBtn.addEventListener('click', () => {
-
-    //     stripe.redirectToCheckout({
-
-    //         sessionId: '<%= sessionId %>'
-
-    //     })
-
-    // })
+function switchModal(modalBtnElem) {
+    modalBtnElem.querySelector('.modal').classList.toggle('d-none')
 }
